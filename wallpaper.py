af2ee80aa1843d1c21d1ffc3ba27a4fb1439eab8
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-08 14:51:49 +0300

    updated to use xml settings gnome background xml background xml wallpaper setup environment variable

--- wallpaper.py
+++ wallpaper.py
@@ -1,8 +1,102 @@
 #!/usr/bin/python3
 import requests
+import os
+
+BASE_DIR = os.path.expanduser("~/.local/share/")
+# Directory with xml that 'points' to xml file with background settings
+GNOME_BACKGROUND_XML_PATH = BASE_DIR + "gnome-background-properties/"
+GNOME_XML = GNOME_BACKGROUND_XML_PATH + "slideshow.xml"
+# Directory with background image and xml
+BACKGROUND_PATH = BASE_DIR + "backgrounds/"
+# Path to background xml
+BACKGROUND_XML = BACKGROUND_PATH + "background.xml"
+# Path to image used as background
+BACKGROUND_IMAGE = BACKGROUND_PATH + "background.jpg"
+
+GNOME_BACKGROUND_XML_CONTENT = \
+    f"""
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+<wallpapers>
+  <wallpaper>
+    <name>{os.getenv("USER")}'s custom slideshow</name>
+    <filename>{BACKGROUND_XML}</filename>
+    <options>zoom</options>
+  </wallpaper>
+</wallpapers>
+"""
+BACKGROUND_XML_CONTENT =\
+    f"""
+<background>
+  <starttime>
+    <year>2024</year>
+    <month>01</month>
+    <day>01</day>
+    <hour>00</hour>
+    <minute>00</minute>
+    <second>00</second>
+  </starttime>
+<!-- This animation will start at midnight. -->
+  <static>
+    <duration>60000.0</duration>
+    <file>{BACKGROUND_IMAGE}</file>
+  </static>
+</background>
+"""
 
 
-res = requests.get("https://picsum.photos/2500")
-with open("/home/lee/.local/share/backgrounds/background.jpg", "wb") as bg:
-    bg.write(res.content)
+def prepare():
+    """
+    create directories for background images and xml files
+    checks whether xml files exist
+    """
+    for path in [GNOME_BACKGROUND_XML_PATH, BACKGROUND_PATH]:
+        os.system("mkdir -p {}".format(path))
 
+    if os.path.isfile(GNOME_XML) and os.path.isfile(BACKGROUND_XML):
+        os.environ["WALLPAPER_SETUP"] = "True"
+        return True
+
+
+def setup():
+    """
+    Setup function
+
+    This function creates the directories for the background images and xml files,
+    and creates the gnome background xml and background xml files with the required content.
+    """
+    if prepare():
+        return
+    # create gnome background xml file
+    with open(GNOME_XML, 'w+', encoding='utf-8') as gxml:
+        gxml.write(GNOME_BACKGROUND_XML_CONTENT)
+
+    # create background xml file
+    with open(BACKGROUND_XML, 'w+', encoding='utf-8') as bgxml:
+        bgxml.write(BACKGROUND_XML_CONTENT)
+
+    os.environ["WALLPAPER_SETUP"] = "True"
+
+
+def main():
+    """
+    Creates a background image for the Gnome desktop background slideshow.
+
+    This function uses the requests library to download an image from the
+    https://picsum.photos/2500 endpoint, and saves the image to the
+    ~/.local/share/backgrounds/background.jpg file.
+
+    Returns:
+        None
+
+    """
+    # create background image
+    res = requests.get("https://picsum.photos/2500")
+    with open(BACKGROUND_IMAGE, "wb") as bg:
+        bg.write(res.content)
+
+
+if __name__ == "__main__":
+    if os.getenv("WALLPAPER_SETUP") != "True":
+        setup()
+    main()
